b184193cd347d021491df85ef3ea44e705c482f9
jonathan <jngbot@gmail.com>
2024-01-23 22:06:11 -0500

    add collision configurations in code

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,25 @@
 extends CharacterBody2D
 
 
-const SPEED = 80.0
-const JUMP_VELOCITY = -250.0
+const BASE_SPEED = 100.0
+const BASE_JUMP_VELOCITY = -200.0
+const BASE_SLIME_CHARGES = 2
 
 @onready var animation_tree : AnimationTree = $AnimationTree
 @onready var sprite : Sprite2D = $Sprite2D
+@onready var character : CharacterBody2D = $Player
 @onready var Slime = preload("res://scenes/slime.tscn")
 @onready var world = get_node("/root/World")
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var last_direction = 1
+var speed = BASE_SPEED
+var jump_velocity = BASE_JUMP_VELOCITY
+var slime_charges = BASE_SLIME_CHARGES
+
+func _ready():
+	set_collision_layer_value(2, true)
+	set_collision_mask_value(2, true)
 
 func update_animation_parameters():
 	if(velocity == Vector2.ZERO):
@@ -34,7 +43,7 @@ func _physics_process(delta):
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -42,9 +51,9 @@ func _physics_process(delta):
 	var direction = sign((get_global_mouse_position() - global_position).x)
 	if direction:
 		last_direction = direction
-		velocity.x = input * SPEED
+		velocity.x = input * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
 
--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,20 +1,26 @@
 extends RigidBody2D
 
+@onready var detonate_timer : Timer = $Self_Detonate_Timer
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var velocity = Vector2.ZERO
 const BASE_MAX_CHARGES = 2
-const BASE_THROW_SPEED = 50
-const BASE_TIME_TO_DETONATE = 5
+const BASE_THROW_SPEED = 150
+const BASE_TIME_TO_DETONATE = 3
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity = BASE_THROW_SPEED * velocity # Replace with function body.
+	velocity = BASE_THROW_SPEED * velocity
+	set_collision_layer_value(1, true)
+	set_collision_mask_value(1, true)
+	detonate_timer.wait_time = BASE_TIME_TO_DETONATE
+	detonate_timer.start()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	move_and_collide(velocity * delta)
 	
-func detonate(target_position):
-	pass
+func detonate():
+	#detonate logic here
+	queue_free()
+
+func _on_self_detonate_timer_timeout():
+	detonate()
